44bf48caa164164936a66768236638fd325990d8
usercaesar773 <140834039+usercaesar773@users.noreply.github.com>
2023-07-30 02:02:03 +0300

    fg.py

--- fg.py
+++ fg.py
@@ -6,13 +6,15 @@ z ='\x1b[38;5;105m'#از
 F = '\033[2;32m' #
 W="\033[0;37m"#ابيض
 Y = '\033[1;34m' #
+os.system('cls'if os.name=='net'else'clear')
 print (p+'''          
 			     | |         | |
 			  ___| |__   __ _| |_
 			 / __| '_ \ / _` | __|
 			| (__| | | | (_| | |_
 			 \___|_| |_|\__,_|\__|''')
-print('')		 
-text=input(W+'you : '+b)
-zz = requests.get(f'https://gptzaid.zaidbot.repl.co/1/text={text}').text
-print(Z+'he  :',f'{Y}',zz)
+print('')
+while True:
+	text=input(W+'you : '+b)
+	zz = requests.get(f'https://gptzaid.zaidbot.repl.co/1/text={text}').text
+	print(Z+'he  :',f'{Y}',zz)
