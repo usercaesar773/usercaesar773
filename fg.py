8fb586145c2383fd36ce9eac752ce903dc0b9292
usercaesar773 <140834039+usercaesar773@users.noreply.github.com>
2023-07-30 01:58:09 +0300

    Create fg.py

---
+++ fg.py
@@ -0,0 +1,18 @@
+import requests
+Z = '\033[1;31m' #احمر
+b ='\x1b[38;5;106m'#
+p = '\x1b[38;5;85m'#اخضر فاتح
+z ='\x1b[38;5;105m'#از
+F = '\033[2;32m' #
+W="\033[0;37m"#ابيض
+Y = '\033[1;34m' #
+print (p+'''          
+			     | |         | |
+			  ___| |__   __ _| |_
+			 / __| '_ \ / _` | __|
+			| (__| | | | (_| | |_
+			 \___|_| |_|\__,_|\__|''')
+print('')		 
+text=input(W+'you : '+b)
+zz = requests.get(f'https://gptzaid.zaidbot.repl.co/1/text={text}').text
+print(Z+'he  :',f'{Y}',zz)
